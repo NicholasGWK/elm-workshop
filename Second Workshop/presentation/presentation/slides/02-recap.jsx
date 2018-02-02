be7533b75838f9407658a5f50be1c8e29507cdd5
Nicholas Kennedy <nick.kennedy@rangle.io>
2018-02-01 23:15:47 -0500

    Up to cmds

--- Second Workshop/presentation/presentation/slides/02-recap.jsx
+++ Second Workshop/presentation/presentation/slides/02-recap.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
-import { Slide, Heading, List, ListItem } from "spectacle";
-
+import {
+  Slide,
+  Heading,
+  List,
+  ListItem,
+  Image,
+  CodePane,
+  Code
+} from "spectacle";
+import elmSnippet from "../../assets/codeSnippets/08-elm-snippet.example";
+import programSnippet from "../../assets/codeSnippets/HtmlProgram.example";
+import Elm from "../../assets/images/Elm.png";
+import ElmTEA from "../../assets/images/ElmTEA.svg";
+import RunTimeCommand from "../../assets/images/ElmRuntime.svg";
 const Recap = [
   <Slide bgColor="primary" textColor="tertiary">
     <Heading size={6} textColor="secondary" caps>
@@ -22,6 +33,18 @@ const Recap = [
       </ListItem>
     </List>
   </Slide>,
+  <Slide>
+    <Heading size={6}> Elm </Heading>
+    <CodePane margin={10} textSize={28} lang="jsx" source={elmSnippet} />
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6}> TEA </Heading>
+    <Image src={Elm} />
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6}> Tagged Unions = Msg</Heading>
+    <Code textSize={30}>type Msg = Increment Int | Search String | Reset</Code>
+  </Slide>,
   <Slide bgColor="primary" textColor="tertiary">
     <Heading size={6} textColor="tertiary" caps>
       Recap example + Exercise!
@@ -33,18 +56,76 @@ const Recap = [
     </Heading>
     <List textSize={36}>
       <ListItem textSize={36} padding={4}>
-        Subscriptions
-      </ListItem>
-      <ListItem textSize={36} padding={4}>
         Commands
       </ListItem>
       <ListItem textSize={36} padding={4}>
-        JS...and ports!
+        Subscriptions
       </ListItem>
       <ListItem textSize={36} padding={4}>
         Deploying Elm
       </ListItem>
+      <ListItem textSize={36} padding={4}>
+        JS...and ports!
+      </ListItem>
     </List>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="tertiary" caps>
+      What are commands?
+    </Heading>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="secondary" caps>
+      Side Effects = Data
+    </Heading>
+    <List textSize={36}>
+      <ListItem textSize={34} padding={4}>
+        In Elm, we can't perform side effects
+      </ListItem>
+      <ListItem textSize={34} padding={4}>
+        Describe what we want, Elm runtime does it
+      </ListItem>
+      <ListItem textSize={34} padding={4}>
+        Just like virtual DOM vs DOM mutation
+      </ListItem>
+      <ListItem textSize={32} padding={4}>
+        Create Cmds with functions, return them in update
+      </ListItem>
+    </List>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6}> TEA </Heading>
+    <Image src={ElmTEA} />
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6}> TEA - Cmds and Subs</Heading>
+    <Image src={RunTimeCommand} />
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="secondary" caps>
+      Not beginners anymore!
+    </Heading>
+    <CodePane margin={20} textSize={28} lang="jsx" source={programSnippet} />
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="tertiary" caps>
+      First side effect - Random stuff!
+    </Heading>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6}>Random.generate</Heading>
+    <Code textSize={30}>generate : (a -> msg) -> Generator a -> Cmd msg</Code>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="tertiary" caps>
+      Generator example - Coin flip!
+    </Heading>
+  </Slide>,
+  <Slide bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="tertiary" caps>
+      Random Number Exercise
+    </Heading>
+    <Code textSize={30}> Random.int: Int -> Int -> Generator Int </Code>
   </Slide>
 ];
 
--- Second Workshop/presentation/presentation/slides/index.js
+++ Second Workshop/presentation/presentation/slides/index.js
@@ -3,40 +3,7 @@ import { cloneElement } from "react";
 
 import Intro from "./01-intro.jsx";
 import Recap from "./02-recap.jsx";
-import RealReason from "./03-realReason.jsx";
-import Outline from "./04-outline.jsx";
-import SpeedBumps from "./05-speedBumps.jsx";
-import Syntax from "./06-syntaxNotes.jsx";
-import HTML from "./07-HTML.jsx";
-import HTMLDemo from "./08-HTMLExplanation.jsx";
-import CSS from "./09-CSS.jsx";
-import TEA from "./11-TEA.jsx";
-import ReduxArch from "./12-ReactRedux.jsx";
-import HTMLProgram from "./13-HtmlProgram.jsx";
-import Msg from "./14-Msg.jsx";
-import Documentation from "./15-Documentation.jsx";
-import TypeAnnotations from "./16-typeAnnotations.jsx";
-import TypeAliases from "./17-typeAlias.jsx";
-import UnionTypes from "./18-UnionTypes.jsx";
 
-const slides = [].concat(
-  Intro,
-  Recap,
-  RealReason,
-  Outline,
-  SpeedBumps,
-  Syntax,
-  HTML,
-  HTMLDemo,
-  CSS,
-  TEA,
-  ReduxArch,
-  HTMLProgram,
-  Msg,
-  Documentation,
-  TypeAnnotations,
-  TypeAliases,
-  UnionTypes
-);
+const slides = [].concat(Intro, Recap);
 
 export default slides.map((slide, i) => cloneElement(slide, { key: i }));
